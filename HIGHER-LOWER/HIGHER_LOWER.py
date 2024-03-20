3e255296105ac4cd4c14035e77caf7e732223aff
stranger <alirezaz313313@gmail.com>
2024-03-20 18:25:25 +0330

    .

--- HIGHER-LOWER/HIGHER_LOWER.py
+++ HIGHER-LOWER/HIGHER_LOWER.py
@@ -4,30 +4,35 @@ import instaloader
 import time
 import clear
 
+# finding the number of followers of instagram accounts.
 L = instaloader.Instaloader()
 user_list = ['nasa', 'cristiano', 'leomessi', 'ufc', 'mmafighting', 'lovethenature95']
+# put them in a dict to call them later.
 data_dict = {user: 0 for user in user_list}
 
 for user in user_list:
     profile = instaloader.Profile.from_username(L.context, user)
     data_dict[user] = profile.followers
 
-
+# making a loop to play the game.
 playing=True
 
 while playing:
+    # positive/negative point to count the ponits and remained chance to guess.
     point_P = 0
     point_N = 0
     chances = 3
+    # make a condition to continue the game.
     while point_P < 3 and point_N < 3 and chances > 0:
         time.sleep(3)
         clear.clear()
         
+        # choose 2 random user from the converted dict to list.
         rand_users = random.sample(list(data_dict.keys()), k=2)
 
         max_followers_user = ""
         max_followers = 0
-
+        # find the user with most followers.
         for user in rand_users:
             if data_dict[user] > max_followers:
                 max_followers_user = user
@@ -37,9 +42,9 @@ while playing:
         print("\n"+rand_users[0])
         print(vs)
         print(rand_users[1]+"\n")        
-            
+        # take input from the player.  
         player=input("enter your guess:")
-
+        # raise error in case the player entered incorrectly.
         if player not in user_list:
             print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
             print("you must enter the profile name.")
@@ -47,7 +52,7 @@ while playing:
             
             
         else:
-            
+            # if the guess was correct add to the positive point and put chances -1.
             if player == max_followers_user:
                 time.sleep(2)
                 print("++++++++++++++++++++++++")
@@ -61,7 +66,7 @@ while playing:
                 
 
 
-                
+            #  if the guess was incorrect add to the negative point and put chances -1.   
             else:
                 time.sleep(2)
                 print("______________")
@@ -73,20 +78,21 @@ while playing:
                 print("!! you have:",chances-1, "more chances.")
                 
                 
-
+        # if positive point was equal to  3 then quit the game and print you win.
         if point_P == 3:
             time.sleep(1)
             print("YOU ARE GOD OF THIS GAME.\n")
             break
+        # if negative point was equal to 3 then quit the game and print you loose.
         elif point_N == 3:
-            print("YOU LOSE THE GAME.")
+            print("YOU LOOSE THE GAME.")
             print(f"the user with the most followers is <<{max_followers_user.upper()}>> with <<{max_followers}>> followers.")
             break
-
+        # if chances was equal to 0 then print you loose the game and quit.    
         if chances ==0:
-            print("you lose the game you have 0 chanses to guess")
+            print("you loose the game you have 0 chanses to guess")
             break
-
+    # ask the user if he want's to replay.if yes continue if not print thanks and quit the game.       
     restart = input("Do you want to restart the game? (Y/N) ").lower()
     if restart != "y":
         clear.clear()
