06f1112a2ca7af1b25961da2af5c97302780dd1f
stranger <alirezaz313313@gmail.com>
2024-03-20 17:46:16 +0330

    final edition

--- HIGHER-LOWER/HIGHER_LOWER.py
+++ HIGHER-LOWER/HIGHER_LOWER.py
@@ -3,40 +3,30 @@ from artt import vs
 import instaloader
 import time
 
-#-------------------------------------------------------------------------------------
-#first part
+# first part
 L = instaloader.Instaloader()
-user_list=['nasa','cristiano','leomessi','ufc','mmafighting','lovethenature95']
-data_dict={
-    "nasa": 0,
-    "cristiano": 0,
-    "leomessi": 0,
-    "ufc": 0,
-    "mmafighting": 0,
-    "lovethenature95": 0,
-}
+user_list = ['nasa', 'cristiano', 'leomessi', 'ufc', 'mmafighting', 'lovethenature95']
+data_dict = {user: 0 for user in user_list}
 
 for user in user_list:
     profile = instaloader.Profile.from_username(L.context, user)
     data_dict[user] = profile.followers
 
+# second part
+point_P = 0
+point_N = 0
+chances = 3
 
-
-#------------------------------------------------------------------------------------
-#second part
-point_P=0
-point_N=0
-
-while point_P<3 and point_N < 3 :
+while point_P < 3 and point_N < 3 and chances > 0:
     
-    rand_users=random.sample(list(data_dict.keys()),k=2)
+    rand_users = random.sample(list(data_dict.keys()), k=2)
 
-    max_followers_user=""
-    max_followers=0
+    max_followers_user = ""
+    max_followers = 0
 
     for user in rand_users:
         if data_dict[user] > max_followers:
-            max_followers_user =user
+            max_followers_user = user
             max_followers = data_dict[user]
             
     print("__________________________________________")        
@@ -61,6 +51,7 @@ while point_P<3 and point_N < 3 :
             print(f"the user with the most followers is <<{max_followers_user.upper()}>> with <<{max_followers}>> followers.")
             point_P+=1
             print("you have",point_P,"points.")
+            print("!! you have:",chances-1, "more chances.")
             
         else:
             time.sleep(2)
@@ -68,28 +59,27 @@ while point_P<3 and point_N < 3 :
             print("OOPS.")
             print("______________")
             print(f"the user with the most followers is <<{max_followers_user.upper()}>> with <<{max_followers}>> followers.")
-
             point_N+=1
+            chances -= 1
+            print("!! you have:",chances-1, "more chances.")
             
-            
-        if point_P== 3 and point_N<3:
-            time.sleep(1)
-            print("YOU ARE GOD OF THIS GAME.")
-            print(f"the user with the most followers is <<{max_followers_user.upper()}>> with <<{max_followers}>> followers.")
-            break
-        elif point_N==3 and point_P<3:
-            print("YOU LOSE THE GAME.")
-            print(f"the user with the most followers is <<{max_followers_user.upper()}>> with <<{max_followers}>> followers.")
-            break
-            
-        # print("__________________________________________")
-        # time.sleep(2)
-        # print("\n"+rand_users[0])
-        # print(vs)
-        # print(rand_users[1]+"\n")
-        # continue
-    
-    restart=input("DO YOU WANT TO REPLAY?(Y/N)").lower()
-    if restart !="y":
+
+    if point_P == 3:
+        time.sleep(1)
+        print("YOU ARE GOD OF THIS GAME.")
+        break
+    elif point_N == 3:
+        print("YOU LOSE THE GAME.")
+        print(f"the user with the most followers is <<{max_followers_user.upper()}>> with <<{max_followers}>> followers.")
         break
 
+    if chances ==0:
+        print("you lose the game you have 0 chanses to guess")
+        break
+
+restart = input("Do you want to restart the game? (Y/N) ").lower()
+if restart == 'y':
+    # Restart the game
+    exec(open('game.py').read())
+else:
+    print("Thanks for playing!")
